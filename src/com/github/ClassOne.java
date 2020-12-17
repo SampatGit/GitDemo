752fb485f8e9e1b1e52aaaf43197d90625cfce6d
Skmsgithub <sampatgit>
2020-12-17 20:27:12 +0530

    commit from offshore branch

--- src/com/github/ClassOne.java
+++ src/com/github/ClassOne.java
@@ -5,6 +5,7 @@ public class ClassOne {
 	public void methodOne() {
 		System.out.println("First method");
 		System.out.println("EXAMPLE OF PULLING CHANGES");
+                System.out.println("IN OFFSHORE BRANCH");
 	}
 
 }
