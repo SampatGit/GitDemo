45a2253cf70eb785820d1d3b617f98d72b39a6c1
Skmsgithub <sampatgit>
2020-12-17 20:50:15 +0530

    final commit

--- src/com/github/ClassOne.java
+++ src/com/github/ClassOne.java
@@ -4,11 +4,8 @@ public class ClassOne {
 	
 	public void methodOne() {
 		System.out.println("First method");
-<<<<<<< HEAD
-		System.out.println("EXAMPLE OF PULLINGs CHANGES");
-=======
+
 		System.out.println("EXAMPLE OF PULLING CHANGESS");
->>>>>>> c1cb9fbdc523d01a4f5e62d33dd536baa5d32d13
 	}
 
 }
