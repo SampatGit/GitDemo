ed9a87e3d673fbbd063d8c1feccb56d3c4476590
Skmsgithub <sampatgit>
2020-12-17 20:33:39 +0530

    verify merge

--- src/com/github/ClassOne.java
+++ src/com/github/ClassOne.java
@@ -4,7 +4,7 @@ public class ClassOne {
 	
 	public void methodOne() {
 		System.out.println("First method");
-		System.out.println("EXAMPLE OF PULLING CHANGES");
+		System.out.println("EXAMPLE OF PULLINGs CHANGES");
 	}
 
 }
